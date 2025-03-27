7244aa6394ec660a7d2c29ecbb1bb3f263305673
Hari Shankar <harishankarn04@gmail.com>
2025-03-27 15:42:18 +0530

    file_path name update

--- Data_load.py
+++ Data_load.py
@@ -4,11 +4,11 @@ from utils.chunk_updater import check_and_update_chunks
 from utils.faiss_store import update_or_create_faiss_vector_store
 from utils.models import embeddings
 
-#csv_file_path = 'Data/sitemap_data.csv'  # Path to your CSV file
-json_file_path = 'Data/sitemap_data.json'  # Path to your JSON file
+#file_path = 'Data/sitemap_data.csv'  # Path to your CSV file
+file_path = 'Data/sitemap_data.json'  # Path to your JSON file
 
 #urls = get_urls_from_csv(csv_file_path)
-urls = get_urls_from_json(json_file_path)
+urls = get_urls_from_json(file_path)
 # Use the function to load and split the data
 docs = check_and_update_chunks(urls, chunk_size=2000, chunk_overlap=400, chunk_file_path="Data/chunks.pkl")
 
